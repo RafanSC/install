a0596b047db11e081b0f0cc8d04bbc96850b3886
RafanSC <153880084+RafanSC@users.noreply.github.com>
2023-12-22 14:20:32 +0700

    Update wes.sh

--- wes.sh
+++ wes.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -q https://raw.githubusercontent.com/RafanSTR/Azerd/ABSTRAK/rafan.sh && chmod +x rafan.sh && ./rafan.sh
+wget raw.githubusercontent.com/RafanSTR/Azerd/ABSTRAK/rafan.sh && chmod +x rafan.sh && ./rafan.sh
 chmod +x wes.sh
