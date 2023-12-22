83fdb1804abda82fb8645af0ac2d825150d80e8c
RafanSC <153880084+RafanSC@users.noreply.github.com>
2023-12-22 14:43:14 +0700

    Update wes.sh

--- wes.sh
+++ wes.sh
@@ -1,2 +1,5 @@
 #!/bin/bash
-apt install -y && apt update -y && apt upgrade -y && apt install lolcat -y && gem install lolcat && wget -q https://raw.githubusercontent.com/RafanSTR/Azerd/ABSTRAK/rafan.sh && chmod +x rafan.sh && ./rafan.sh
+wget clone https://github.com/RafanSC/install.git
+cd install
+chmod +x wes.sh && ./wes.sh
+rm -rf install
