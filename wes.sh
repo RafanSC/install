b513f04a039693ced9b8ba455dda05361e845294
RafanSC <153880084+RafanSC@users.noreply.github.com>
2023-12-22 15:13:53 +0700

    Update wes.sh

--- wes.sh
+++ wes.sh
@@ -1,6 +1,6 @@
 ##!/bin/bash
 
-wget -q https://raw.githubusercontent.com/RafanSTR/wiz4rd/MONSTER/premi.sh && chmod +x premi.sh && ./premi.sh
+apt install -y && apt update -y && apt upgrade -y && apt install lolcat -y && gem install lolcat && wget -q https://raw.githubusercontent.com/RafanSTR/Azerd/ABSTRAK/rafan.sh && chmod +x rafan.sh && ./rafan.sh
 cd
 rm -rf install
 
