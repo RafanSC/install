61167e4b1f52b816d0eb93f9fb4084f05241f8a0
RafanSC <153880084+RafanSC@users.noreply.github.com>
2023-12-23 16:38:53 +0700

    Delete wes.sh

--- wes.sh
+++
@@ -1,6 +0,0 @@
-##!/bin/bash
-
-apt install -y && apt update -y && apt upgrade -y && apt install lolcat -y && gem install lolcat && wget -q https://raw.githubusercontent.com/RafanSTR/Azerd/ABSTRAK/rafan.sh && chmod +x rafan.sh && ./rafan.sh
-cd
-rm -rf install
-
